b4cc38e603a4b5e868e146647162ccadded8c913
dhairya218 <dhairykdandaiya@gmail.com>
2025-06-02 16:16:02 +0530

    Restructure project to root directory

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    };
+    return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['localhost', 'coregenesis-backend.onrender.com'],
+    unoptimized: true,
+  }
+};
+
+module.exports = nextConfig; 
