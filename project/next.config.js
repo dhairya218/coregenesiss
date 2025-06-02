5f9cea223d3c0af39c96248512f62a34d9d56936
dhairya218 <dhairykdandaiya@gmail.com>
2025-06-02 15:18:41 +0530

    Fix path alias resolution in Next.js config

--- project/next.config.js
+++ project/next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname),
+    };
+    return config;
+  },
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
